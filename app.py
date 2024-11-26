635c99adf7b2fa23a28fac1c95ad514c2959390e
Ricardo Felipe <ricardofelipecrvg@gmail.com>
2024-11-26 18:29:28 -0300

    Iniciando experimento e CRUD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,16 @@ def home ():
 @app.route('/imagens')
 def imagens():
     return render_template('imagens.html')
+
+@app.route('/desabafo', methods=['GET', 'POST'])
+def desabafo ():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        assunto = request.form['assunto']
+        desabafo = request.form['desabafo']
+
+        return render_template('desabafo.html', nome=nome, assunto=assunto, desabafo=desabafo)
+    return render_template('desabafo.html', nome=None, assunto=None, desabafo=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
