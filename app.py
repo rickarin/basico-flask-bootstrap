d53c6b085fe8e412190d58cba17968bc5992bcca
rickarin <ricardofelipecrvg@gmail.com>
2024-11-27 14:38:20 -0300

    Criação de arquivos para teste de Variaveis e Condicionais

--- app.py
+++ app.py
@@ -21,5 +21,11 @@ def desabafo ():
         return render_template('desabafo.html', nome=nome, assunto=assunto, desabafo=desabafo)
     return render_template('desabafo.html', nome=None, assunto=None, desabafo=None)
 
+@app.route('/teste')
+def teste() :
+    nome = 'Arin'
+    teste = True
+    return render_template('teste.html', nome = nome, teste = teste)
+
 if __name__ == '__main__':
     app.run(debug=True)
